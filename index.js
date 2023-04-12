41abc2cdd8d57ecbaaa1d03334f55e2121c59e9d
Doolyawit Yodkaew <dew.doolyawit@gmail.com>
2023-04-12 20:21:30 +0700

    add delete button

--- index.js
+++ index.js
@@ -1,23 +1,26 @@
-//Variable
+// ----Variable----
 const inputBtn = document.getElementById("input-btn"); //  input button
 const inputEl = document.getElementById("input-el"); // input field
 const ulEl = document.getElementById("ul-el"); // ul list for outout
+const deleteBtn = document.getElementById("delete-btn"); // delete button
 let myLeads = []; // array for store links
+const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads")); // get localstorage data
 
-inputBtn.addEventListener("click", function () {
-  // when click input button
-  myLeads.push(inputEl.value); // insert input field data into array
-  renderLeads(); // call show output function
-  inputEl.value = ""; // clear input field
-});
-function renderLeads() {
+// ----In case we refresh page ( have data already , not first time use)----
+if (leadsFromLocalStorage) {
+  // check if have previous local data
+  myLeads = leadsFromLocalStorage; // set to array for send to renderLeads();
+  render(myLeads); // render it out
+}
+//----Show output----
+function render(leads) {
   let listItems = ""; // set list (of links) blank every loop
-  for (i = 0; i < myLeads.length; i++) {
+  for (i = 0; i < leads.length; i++) {
     listItems += `<li>
-        <a href="https://${myLeads[i]}"target='_blank'>
-        ${myLeads[i]}
-        </a>
-    </li> `; // create li tag for each link
+          <a href="https://${leads[i]}"target='_blank'>
+          ${leads[i]}
+          </a>
+      </li> `; // create li tag for each link
     //Another way
     //   1.create element
     //   const li = document.createElement("li");
@@ -28,3 +31,19 @@ function renderLeads() {
   }
   ulEl.innerHTML = listItems; // add li tag to ul
 }
+
+// ----Click Save----
+inputBtn.addEventListener("click", function () {
+  // when click input button
+  myLeads.push(inputEl.value); // insert input field data into array
+  inputEl.value = ""; // clear input field
+  localStorage.setItem("myLeads", JSON.stringify(myLeads)); // store in local storage (convert array --> string)
+  render(myLeads); // call show output function
+});
+
+// ----Click Delete All----
+deleteBtn.addEventListener("dblclick", function () {
+  localStorage.clear();
+  myLeads = []; // next loop
+  render(myLeads); // for output
+});
